25a8bdcdddfcfa31d464c362086a2b14db4aa0d1
rgranatodutra <r.granatodutra@gmail.com>
2023-03-28 01:58:22 -0300

    fix: variable name

--- src/controllers/users/recover.controller.ts
+++ src/controllers/users/recover.controller.ts
@@ -3,7 +3,7 @@ import { User } from "../../entities/user.entity";
 import services from "../../services";
 
 export const recoverUserController = async (req: Request, res: Response) => {
-    const deletedUser: User = await services.users.recover(req.findUser);
+    const recoveredUser: User = await services.users.recover(req.findUser);
 
-    return res.status(200).json(deletedUser);
+    return res.status(200).json(recoveredUser);
 };
