b2ddf5daf958c1ae74512f6e11e1b4a42e1c9076
juniomatias01 <juniormatias20111@gmail.com>
2023-04-12 07:28:11 -0300

    feat: job attendance

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import app from "./app";
 import { AppDataSource } from "./data-source";
 import WebSocket from "./WebSocket/WebSocket";
 import WhatsappWeb, { getRunningAttendances } from "./WebSocket/WhatsappClient";
+import { cron } from "./job/updateAttendanceStatus.job";
 
 const PORT: number = Number(process.env.PORT) || 8000;
 const SOCKET_PORT: number = Number(process.env.SOCKET_PORT) || 5000;
@@ -24,5 +25,6 @@ async function initialize () {
     await WhatsappWeb.initialize().then(_ => console.log("Whatsapp Initialized"));
     getRunningAttendances();
 };
+cron;
 
 initialize();
