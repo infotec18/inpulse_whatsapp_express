dda145a95b038d994010774db0002f884522aa4d
rgranatodutra <r.granatodutra@gmail.com>
2023-04-12 11:55:37 -0300

    feat: agendamentos

--- src/job/updateAttendanceStatus.job.ts
+++ src/job/updateAttendanceStatus.job.ts
@@ -1,6 +1,5 @@
 import { updateAttendanceStatus } from "../services/attendances/scheduleAttendanceStatus.service";
-
-export const cron = require('node-cron');
+import * as cron from "node-cron";
 
 // Define o cron job para ser executado a cada 15 minutos
 cron.schedule('*/15 * * * *', () => {
