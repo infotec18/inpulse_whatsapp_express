b660e2cf5a3e4c5fcba31dc24d68adeeaf1a2101
alvaroeduardo <alvaroedusilva@yahoo.com>
2023-04-19 08:55:30 -0300

    hfix: corrigindo bugs solicitados

--- src/controllers/customers/delete.controller.ts
+++ src/controllers/customers/delete.controller.ts
@@ -3,7 +3,8 @@ import services from "../../services";
 import { Customer } from "../../entities/customer";
 
 export const deleteCustomerController = async (req: Request, res: Response) => {
-    const customer: Customer = await services.customers.softDelete(req.findCustomer);
+    console.log(req.findCustomer)
+    const customer = await services.customers.softDelete(req.findCustomer);
 
     return res.status(200).json(customer);
 }
--- src/services/customers/create.service.ts
+++ src/services/customers/create.service.ts
@@ -3,14 +3,20 @@ import { Repository } from "typeorm";
 import { Customer } from "../../entities/customer";
 import { AppDataSource } from "../../data-source";
 import services from "..";
+import { AppError } from "../../errors";
 
 export async function createCustomerService(req: Request): Promise<Customer> {
+    const customersRepository: Repository<Customer> = AppDataSource.getRepository(Customer);
+   
+    const existsCustomer = await customersRepository.findOneBy({CPF_CNPJ: req.body.CPF_CNPJ});
+
+    if(!!existsCustomer) throw new AppError("Não é possível registrar esse CNPJ pois já existe.", 400);
+   
     const n = new Date();
     const DATA_CAD = `${n.getFullYear()}-${n.getMonth()}-${n.getDate()} ${n.getHours()}:${n.getMinutes()}`;
 
     const lastId = await services.customers.getLastId();
 
-    const customersRepository: Repository<Customer> = AppDataSource.getRepository(Customer);
 
     const newCustomer = await customersRepository.save({...req.body, CODIGO: lastId + 1, DATA_CAD });
 
