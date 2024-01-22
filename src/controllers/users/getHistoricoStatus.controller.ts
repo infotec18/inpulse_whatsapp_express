3a5c91901c950b9956ba5bf2bae6d0621b056941
juniomatias01 <juniormatias20111@gmail.com>
2024-01-22 18:36:03 -0300

    fix

--- src/controllers/users/getHistoricoStatus.controller.ts
+++ src/controllers/users/getHistoricoStatus.controller.ts
@@ -5,11 +5,10 @@ export const getHistoricoStatusController = async (req: Request, res: Response) 
 
     // if(!req.user.isAdmin) return res.status(401).json({ message: "No authorization" })
 
-    const startDate = req.query.startDate ? new Date(req.query.startDate as string) : new Date(Date.now() - (1000 * 60 * 60 * 24 * 1));
-    const endDate = req.query.endDate ? new Date(req.query.endDate as string) : new Date(Date.now())
+    const startDate = req.query.startDate !== 'null' ? new Date(req.query.startDate as string) : new Date();
+    const endDate = req.query.endDate !== 'null' ? new Date(req.query.endDate as string) : new Date();    
     const codigo_operador = req.query.codigo_operador ? req.query.codigo_operador  : 0;
-
     const { dados } = await services.users.getHistoricoStatus(startDate, endDate, Number(codigo_operador));
-  
+
     return res.status(201).json({ dados });
 };
