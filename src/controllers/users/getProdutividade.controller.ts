5cd924b4ed96cab4d494cf3ae914243336354b5c
juniomatias01 <juniormatias20111@gmail.com>
2024-01-25 22:45:31 -0300

    fix

--- src/controllers/users/getProdutividade.controller.ts
+++ src/controllers/users/getProdutividade.controller.ts
@@ -5,8 +5,15 @@ export const getProdutividadeController = async (req: Request, res: Response) =>
 
     // if(!req.user.isAdmin) return res.status(401).json({ message: "No authorization" })
 
-    const startDate = req.query.startDate ? new Date(req.query.startDate as string) : new Date(Date.now() - (1000 * 60 * 60 * 24 * 1));
-    const endDate = req.query.endDate ? new Date(req.query.endDate as string) : new Date(Date.now())
+    const currentDate = new Date();
+    const currentUTC = currentDate.getTime() + currentDate.getTimezoneOffset() * 60000;
+    const currentDateTimeMinus3 = new Date(currentUTC + (3600000 * -3));
+
+    const endDate = req.query.endDate ? new Date(req.query.endDate as string) : currentDateTimeMinus3;
+
+    const yesterday = new Date(currentDateTimeMinus3);
+    yesterday.setDate(currentDateTimeMinus3.getDate() - 1);
+    const startDate = req.query.startDate ? new Date(req.query.startDate as string) : new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate(), 23, 59, 59, 999);
 
     const { dados } = await services.users.getProdutividade(startDate, endDate);
   
